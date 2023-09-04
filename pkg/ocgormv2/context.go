cab24ec670b1901a2c226f8c04b98b5e0d939e63
Tiago Scolari <tiago@hashicorp.com>
2023-09-04 13:08:32 +0100

    Add support to gormv2
    Create the ocgormv2 package which uses the new gormv2 version and import
    paths.

---
+++ pkg/ocgormv2/context.go
@@ -0,0 +1,12 @@
+package ocgormv2
+
+import (
+	"context"
+
+	"gorm.io/gorm"
+)
+
+// WithContext sets the current context in the db instance for instrumentation.
+func WithContext(ctx context.Context, db *gorm.DB) *gorm.DB {
+	return db.New().Set(contextScopeKey, ctx)
+}
