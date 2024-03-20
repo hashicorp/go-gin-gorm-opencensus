729612ffe11c0690ccdb6b583540e343c3317e3d
Dmitry Malchikov <dmitry.malchikov@hashicorp.com>
2024-03-20 14:18:05 +0100

    Update context to use GORM v2 API
    Refactored the WithContext function to use the new context handling
    provided by gorm v2, updating import paths and the context setting
    method accordingly.

--- pkg/ocgormv2/context.go
+++ pkg/ocgormv2/context.go
@@ -1,12 +1,13 @@
-package ocgorm
+package ocgormv2
 
 import (
 	"context"
 
-	"github.com/jinzhu/gorm"
+	"gorm.io/gorm"
 )
 
 // WithContext sets the current context in the db instance for instrumentation.
+// Deprecated: prefer direct usage of db.WithContext(ctx).
 func WithContext(ctx context.Context, db *gorm.DB) *gorm.DB {
-	return db.New().Set(contextScopeKey, ctx)
+	return db.WithContext(ctx)
 }
